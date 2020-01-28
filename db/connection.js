5e5946fbda615f5a18b4048704410e84d17a75d8
majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-01-28 18:34:26 -0500

    changed env var

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
-  mongoURI =
-    "mongodb+srv://dan:c0ins89@cluster0-pp109.mongodb.net/test?retryWrites=true&w=majority";
+  mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/ssc";
 }
